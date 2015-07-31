d8afbc90c57bf4111c5d3901d8a7ced2da177b19
tianqiu <tianqiu@hustunique.com>
2015-07-31 20:02:37 +0800

    +

--- str.c
+++ str.c
@@ -2,22 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-char * deal(char *a);
+char * dealrequest(char *a);
 
 
-char * deal(char *request)
+char * dealrequest(char *request)
 {
-	char *n=NULL;
-	//char b[]="qqqqddssddsdsadaasddddddddddddddddddddddddddddddddddqqqqqreqqqq";
-	//n=(char *)malloc(sizeof(b));
-	//strcpy(n,b);
-	printf("%s\n",request);
-	char *p;
-	p=strtok(request,"8");
-	printf("\n%s\n",p );
+	char *method=NULL;
+	char *url=NULL;
 
-	p=strtok(NULL,"8");
-	printf("\n%s\n",p );
+	char *n=NULL;
+	printf("%s\n",request);
+	method=strtok(request," ");
+	url=strtok(NULL," ");
+	if(!url)
+	{
+		return n;
+	}
+	else
+	{
+		
+	}
+	
+	
+	if(strcmp(method,"GET")==0)
+	{
+		printf("%s",url);
+	}	
 	return n;
 }
 
@@ -26,7 +36,7 @@ int main ()
 {
 char a[]={"GET / HTTP/1.1\r\nHost: 127.0.0.1:8888\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0\r\n\r\n"};
 char *c;
-c=deal(a);
+c=dealrequest(a);
 //printf("c=%s\n",c);
 free(c);
 return 0;
