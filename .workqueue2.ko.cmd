ebb552b01dd7164b3447cf5bf0604a2f2f6c61ed
tianqiu <tianqiu@hustunique.com>
2015-08-07 20:49:56 +0800

    +

--- .workqueue2.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/qiutian/c/workqueue2.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/qiutian/c/workqueue2.ko /home/qiutian/c/workqueue2.o /home/qiutian/c/workqueue2.mod.o
