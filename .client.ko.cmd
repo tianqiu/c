56e9e6391a8f6a69e365eb9a58a4dba919032f20
tianqiu <tianqiu@hustunique.com>
2015-08-09 13:55:36 +0800

    +

--- .client.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/qiutian/c/client.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/qiutian/c/client.ko /home/qiutian/c/client.o /home/qiutian/c/client.mod.o
--- .shell.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/qiutian/c/shell.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/qiutian/c/shell.ko /home/qiutian/c/shell.o /home/qiutian/c/shell.mod.o
